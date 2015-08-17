292dfba4e6abda99c0f0ea10bfe0a27fa9ffa24b
mengqiang <mengqiang.q@gmail.com>
2015-08-17 19:49:11 +0800

    fix   project

--- HeroJson/Mysql/GetUserInfo.js
+++ HeroJson/Mysql/GetUserInfo.js
@@ -1,28 +1,28 @@
 ﻿var database = require('D:/Node.js/HeroJson/HeroJson/Mysql/ConnectMysql.js');
 
 exports.Response = function (req , res) {
-    
+	
 	var connection = database.getConnection();
-    
-    var sqlUser = 'select UserId,Gold,Diamond,Stamina from tb_userinfo where userId = ' + connection.escape(req.body.userId);
-    var sqlHero = 'select HeroId,HeroLevel from tb_userhero where userId = ' + connection.escape(req.body.userId);
-    var sqlEquipment = 'select * from tb_userequipment where userId = ' + connection.escape(req.body.userId);
+	
+	var sqlUser = 'select UserId,Gold,Diamond,Stamina from tb_userinfo where userId = ' + connection.escape(req.body.userId);
+	var sqlHero = 'select HeroId,HeroLevel from tb_userhero where userId = ' + connection.escape(req.body.userId);
+	var sqlEquipment = 'select * from tb_userequipment where userId = ' + connection.escape(req.body.userId);
 
-    var pool = database.getConnectionPool();
-    
-    pool.getConnection(function (err, connection) {
-	     var  respondResult = "" ;
-        // Use the connection
-        connection.query(sqlUser, function (err, rows) {
-            // And done with the connection.
-            var result = JSON.stringify(rows[0]);
+	var pool = database.getConnectionPool();
+	
+	pool.getConnection(function (err, connection) {
+		 var  respondResult = "" ;
+		// Use the connection
+		connection.query(sqlUser, function (err, rows) {
+			// And done with the connection.
+			var result = JSON.stringify(rows[0]);
 			//  connection.release();
-	        respondResult += "\"data\": " + result+",";
+			respondResult += "\"data\": " + result+",";
 			
 			
 			
 			connection.query(sqlHero, function (err, rows) {
-            
+			
 				var result = JSON.stringify(rows);
 				// connection.release();
 				respondResult += "\"heroConfig\": " + result+ ",";
@@ -35,9 +35,9 @@ exports.Response = function (req , res) {
 					res.type('json');
 					respondResult = "{" + respondResult +"}";
 					return res.send(respondResult);
-                });
+				});
 			});
-         });
-     });
+		 });
+	 });
 
 }
