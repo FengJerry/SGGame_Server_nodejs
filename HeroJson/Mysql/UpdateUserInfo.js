1d79c1d3d3a17c1931038aac4f545e092bd308c9
fengjietang <fengjietang@foxmail.com>
2015-08-25 12:39:58 +0800

    no message

--- HeroJson/Mysql/UpdateUserInfo.js
+++ HeroJson/Mysql/UpdateUserInfo.js
@@ -7,12 +7,12 @@ exports.Response = function (req , res) {
 	var sqlUpdate = 'update tb_userinfo set ' + checkGold() 
 											  + checkDiamond()
 											  + checkStamina()
-                                              + 'where UserId = ' + connection.escape(req.body.userId);
+                                              + 'where UserId = ' + req.body.userId;
 	
 	
 	function checkGold(){
 			    if (!(typeof req.body.gold  === 'undefined') ) {
-					return 'Gold = Gold + ' + connection.escape(req.body.gold) + ' ';
+					return 'Gold = Gold + ' + req.body.gold + ' ';
 				}
 				else{
 				return '';
@@ -24,27 +24,78 @@ exports.Response = function (req , res) {
 				if(checkGold() !== '')
 				result = ',';
 			    if (!(typeof req.body.diamond  === 'undefined') ) {
-					result = result + 'Diamond = Diamond + ' + connection.escape(req.body.diamond)+ ' ';
+					result = result + 'Diamond = Diamond + ' + req.body.diamond+ ' ';
 					return result;
 				}
 				else{
 				return '';
 				}
 	}
-	
-	function checkStamina(){
-					var result = '';
-				if(checkDiamond() !== '')
-				result = ',';
-			    if (!(typeof req.body.stamina  === 'undefined') ) {
-					result = result + 'Stamina = Stamina + ' + connection.escape(req.body.stamina)+ ' ';
-					return result;
-				}
-				else{
-				return '';
-				}
+
+	function checkStamina() {
+	    var result = '';
+	    if (checkDiamond() !== '')
+	        result = ',';
+	    if (!(typeof req.body.stamina === 'undefined')) {
+	        result = result + 'Stamina = Stamina + ' + req.body.stamina + ' ';
+	        return result;
+	    }
+	    else {
+	        return '';
+	    }
+	}
+
+	function checkPowder() {
+	    var result = '';
+	    if (checkStamina() !== '')
+	        result = ',';
+	    if (!(typeof req.body.powder === 'undefined')) {
+	        result = result + 'Powder = Powder + ' + req.body.powder + ' ';
+	        return result;
+	    }
+	    else {
+	        return '';
+	    }
 	}
 	
+	function checkGreenFragment() {
+	    var result = '';
+	    if (checkPowder() !== '')
+	        result = ',';
+	    if (!(typeof req.body.greenfragment === 'undefined')) {
+	        result = result + 'GreenFragment = GreenFragment + ' + req.body.greenfragment + ' ';
+	        return result;
+	    }
+	    else {
+	        return '';
+	    }
+	}
+
+	function checkBlueFragment() {
+	    var result = '';
+	    if (checkGreenFragment() !== '')
+	        result = ',';
+	    if (!(typeof req.body.bluefragment === 'undefined')) {
+	        result = result + 'BlueFragment = BlueFragment + ' + req.body.bluefragment + ' ';
+	        return result;
+	    }
+	    else {
+	        return '';
+	    }
+	}
+	function checkPurpleFragment() {
+	    var result = '';
+	    if (checkBlueFragment() !== '')
+	        result = ',';
+	    if (!(typeof req.body.purpleFragment === 'undefined')) {
+	        result = result + 'PurpleFragment = PurpleFragment + ' + req.body.purpleFragment + ' ';
+	        return result;
+	    }
+	    else {
+	        return '';
+	    }
+	}
+
     var pool = database.getConnectionPool();
     
     pool.getConnection(function (err, connection) {
