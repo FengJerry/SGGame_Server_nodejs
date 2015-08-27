41c0e1498e9f12c5c5d9a04c0c3723a6a2eef52f
fengjietang <fengjietang@foxmail.com>
2015-08-27 10:12:18 +0800

    fixed duplicate hero upgrade

--- HeroJson/Mysql/HeroUpgrade.js
+++ HeroJson/Mysql/HeroUpgrade.js
@@ -14,7 +14,7 @@ exports.Response = function (req , res) {
                                                                                                                     + queryUpgradeTime
                                                                                                                     + '),NOW())'
                                                                                                                     + ',0)'
-
+    var checkExist = 'SELECT EXISTS(SELECT * FROM tb_heroupgrade WHERE UserHeroId = ' + req.body.userHeroId + ') as exist';
     var pool = database.getConnectionPool();
     
     pool.getConnection(function (err, connection) {
@@ -23,17 +23,38 @@ exports.Response = function (req , res) {
             throw err;
         }
 				
+        connection.query(checkExist, function (err, rows) {
+            // And done with the connection.
+            if (err) {
+                console.log("mysql checkExist error" + err);
+                connection.release();
+                return res.send(err);
+            }
+
+            if (rows[0].exist.toString() == 1) {
+                connection.release();
+                return res.send("Hero is upgrading!");
+            }
+            else
+            {
                 connection.query(insertUpgradeHero, function (err, rows) {
                     // And done with the connection.
                     if (err) {
                         console.log("mysql upgradeHero error" + err);
-						connection.release();
+                        connection.release();
                         return res.send(err);
                     }
                     connection.release();
 
                     return res.send("Request successed!");
                 });
+
+            }
+
+
+        });
+
+     
      });
 
 }
