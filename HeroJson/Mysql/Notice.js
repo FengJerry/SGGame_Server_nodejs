5a6f51fb11d7983772a1fd2c6d0d35df8905f8c5
mengqiang <mengqiang.q@gmail.com>
2015-08-28 20:25:48 +0800

    add  notice interface

---
+++ HeroJson/Mysql/Notice.js
@@ -0,0 +1,32 @@
+﻿var database = require('./ConnectMysql.js');
+
+
+exports.GetResponse = function (req , res) {
+    
+    
+    
+    var sql = 'select top 10 from tb_news'
+    
+    var pool = database.getConnectionPool();
+    
+    pool.getConnection(function (err, connection) {
+        // Use the connection
+        if (err) {
+            throw err;
+        }
+        
+        connection.query(sql, function (err, rows) {
+            // And done with the connection.
+            
+            var result = JSON.stringify(rows);
+            
+            connection.release();
+            
+            
+            res.type('json');
+            return res.send("{ \"data\": " + result + "}");
+            
+        });
+    });
+    
+}
